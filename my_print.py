e8711f12503e56ae9287559c86631997329fdcde
John Park <johnpa2@cisco.com>
2020-10-10 23:07:49 -0500

    added printer file

--- my_print.py
+++ my_print.py
@@ -1,22 +1,4 @@
-'''
-AUTHOR
-John Park (john-yohan-park)
-
-
-INSTRUCTIONS
-1. put this file in the same directory as your working file
-2. import its usable functions: `from my_print import *`
-
-
-USABLE FUNCTIONS
-
-print_time(func, param 1, param2, ...)
-    WHAT IT DOES    print function's result and runtime
-                
-print_var(some_variable)
-    WHAT IT DOES    print variable's name and value  
-
-'''
+# By John Park (john-yohan-park)
 
 import time
 
